69c00bca28cf19ef8a9e5cf8eb01d7e4ee887bb1
kevinwilliamscbh <168110431+kevinwilliamscbh@users.noreply.github.com>
2025-02-18 06:02:01 -0500

    Add files via upload

--- TeamsPermissions.psm1
+++ TeamsPermissions.psm1
@@ -1,170 +1,26 @@
 ﻿
-<#********************************************************************
-Export Teams permissions,
-store export in Blob Container
 
-Usage: TeamsPermissions -SharedAccessToken 0000-0000-0000-0000-000 -BaseURI "https:/storage/container" -ImportFile "https:/storage/container/ImportFile.csv?SAS" | "C:/localfile"
+$URI = 'https://raw.githubusercontent.com/kevinwilliamscbh/m365MigrationScripts/refs/heads/main/TeamsPermissions.psm1'
+$Script = [ScriptBlock]::Create((new-object Net.WebClient).DownloadString($URI))
+$ClientCode = 'CBH'
 
--SharedAccessToken (required)
--BaseURI (optional; if not supplied, will use hardcoded value)
--ImportFile (optional; if supplied, will override $teamMailNickName variable)
-   ImportFile should contain a single MailNickName per line, with no header
-   Include SA key in filename if required
+$SAS = 'sp=racw&st=2025-02-17T18:05:17Z&se=2025-02-22T02:05:17Z&spr=https&sv=2022-11-02&sr=c&sig=ZA6VqImomwOjpwoSSkoLxVZDphD5GJsrNA8ETJisrpI%3D'
+$ImportFile = 'https://raw.githubusercontent.com/kevinwilliamscbh/m365MigrationScripts/refs/heads/main/MyTeamsImportFile.csv'
+$ExportFile = ''
+$BaseUri = 'https://bittitanmigrationangeion.blob.core.windows.net/uploaddata'
+#$MailNickNames = 'ProjectDemo12,DemoProject4,ScottMaddenTestDemo,ComplexProcessManagementDemo,ManagedServices,SMProjectTemplate'
+$MailNickNames = '' 
+                    
+$Arguments = @($SAS, $ClientCode, $ImportFile, $ExportFile, $BaseUri, $MailNickNames)
+Invoke-command -ScriptBlock $Script -ArgumentList $Arguments
 
-Kevin Williams
-CBH
-Updated 17FEB2025
 
-*********************************************************************#>
-
-#Require Shared Access Token
-param (
-  [Parameter(Mandatory)][string]$SharedAccessToken,
-  [string]$ClientCode = "CBH",
-  [string]$ImportFile,
-  [string]$BaseUri,
-  [string]$MailNickNames
-)
-
-$ErrorActionPreference = "Stop"
-$exportFile = "Team,MailNick,Channel,UPN,DisplayName,Role`n"
-$teams = @()
-$readTeams = $false
-$timeStamp = (Get-Date).ToString("yyMMdd_HHmm")
-$exportFileName = $ClientCode + "_TeamsPermissions-" + $timeStamp + ".csv"
-$exportFileUri = "$BaseUri/$exportFileName" + "?" + $SharedAccessToken
-
-#Check if NickNames passed in arguments or if need to read
-If ($MailNickNames -ne "")
-    {
-    $MailNickNames = $MailNickNames.Replace(" ","")
-    $MailNickNames = $MailNickNames.Replace(","," ")
-    $teamMailNickNames = [array]$MailNickNames.Split(" ")
-    }
-else
-    {
-    $readTeams = $true
-    }
-
-#Check to see if file import required
-If ($ImportFile.Length -gt 0)
-    {
-    If ($ImportFile.Substring(0,5) -eq "https")
-        {
-        $teamMailNickNames = (Invoke-WebRequest -Uri $ImportFile -Method Get).Content
-        }
-    else
-        {
-        $teamMailNickNames = Import-CSV -Path $ImportFile
-        }
-    }
-else
-    {
-    if($teamMailNickNames.Length -eq 0)
-        {
-        $readTeams = $true
-        }
-    }
-
-#Connect to MS Teams PowerShell
-Try
-    {
-    Get-Team
-    }
-    catch
-        {
-        If ($PSVersionTable.PSEdition -eq "Desktop")
-            {
-            Connect-MicrosoftTeams 
-            }
-            else
-            {
-            Connect-MicrosoftTeams -UseDeviceAuthentication
-            }
-        }
-
-If ($readTeams)
-    {
-    Write-Host "Reading all Teams from tenant" -Foreground Yellow
-    $teamMailNickNames = (Get-Team).MailNickName
-    }
-
-#Obtain Team Channels
-ForEach($team in $teamMailNickNames)
-    {
-    $addTeam = $true
-    try
-        {
-        $teamInfo = Get-Team -MailNickName $team
-        }
-        catch
-            {
-            Write-Host "Error retrieving Team: $($team)" -ForegroundColor Red
-            Write-Host "Error Message: $($_.Exception.Message)" -ForegroundColor Red
-            $addTeam = $false
-            }
-    try
-        {
-        $channels = [array](Get-TeamAllChannel -GroupId $teamInfo.GroupID).DisplayName
-        }
-        catch
-            {
-            Write-Host "Error retrieving Channels for: $($team)" -ForegroundColor Red
-            Write-Host "Error Message: $($_.Exception.Message)" -ForegroundColor Red
-            $addTeam = $false   
-            }  
-    $teamObj = New-Object PSObject -Property @{
-        GroupID     = $teamInfo.GroupID
-        DisplayName = $teamInfo.DisplayName
-        MailNick    = $team
-        Channels    = $channels
-        }
-    If($addTeam)
-        {
-        $teams += $teamObj
-        }
-    }
-
-#Retrieve members and update export data
-ForEach($team in $teams)
-    {
-    ForEach($channel in $team.channels)
-        {
-        $users = Get-TeamChannelUser -GroupID $team.GroupID -DisplayName $channel
-        Write-Host "Processing Channel $channel in Team" $team.DisplayName
-        ForEach($user in $users)
-            {
-            $outLine = $team.DisplayName + "," + $team.MailNick + "," + $channel + "," + $user.User + "," + $user.Name + "," + $user.Role + "`n"
-            $exportFile += $outLine
-            Write-Host $outLine
-            }
-        }
-    }
-
-#Upload file to Angeion File Share and verify
-$headers = @{'x-ms-blob-type' = 'BlockBlob'}
-try
-    {
-    Invoke-RestMethod -Uri $exportFileUri -Method Put -Body $exportFile -Headers $headers
-    }
-    catch
-        {
-        Write-Host "Error Message: $($_.Exception.Message)" -ForegroundColor Yellow
-        Write-Host "ExportFileURI:" $exportFileUri
-        throw "File upload failed"
-        }
-try
-    {
-    $check = Invoke-RestMethod -Uri $exportFileUri -Method Get -Headers $headers
-    }
-    catch
-        {
-        Write-Host "Unable to verify file upload" -ForegroundColor Red
-        Write-Host "Error Message: $($_.Exception.Message)" -ForegroundColor Yellow
-        pause
-        }
-If ($check -eq $exportFile)
-    {
-    Write-Host "File $exportFileName successfully uploaded" -ForegroundColor Yellow
-    Disconnect-MicrosoftTeams -Confirm:$false
-    }
+$ClientCode = 'CBH'
+$SAS = ''
+$ImportFile = ''
+$ExportFile = ''
+$BaseUri = ''
+$MailNickNames = 'ProjectDemo12'
+$Arguments = @($ClientCode, $SAS, $ImportFile, $ExportFile, $BaseUri, $MailNickNames)
+#cd 'c:\Users\Kevin.Williams\OneDrive - Cherry Bekaert\Scripts\MigrationScripts'
+.\TeamsPermissions.ps1 @Arguments
